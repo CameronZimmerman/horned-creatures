6e7c36b11f0460a3ae906bec0a6efb7e62d7309e
Cameron A Zimmerman <cameronazimmerman@gmail.com>
2021-02-09 15:44:05 -0800

    add selection component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import './App.css';
 import data from './data.js';
 import React, { Component } from 'react';
 import ImageList from './ImageList/ImageList.js';
-
+import Selection from './Selection/Selection.js';
 
 export default class App extends Component {
   render() {
     return (
       <div className="App">
+        <section className = "selection Form">
+          <form>
+            <Selection selectionName = "Keyword" property = "keyword" imageData = {data}/>
+            <Selection selectionName = "Horns" property = "horns" imageData = {data}/>
+          </form>
+        </section>
         <ImageList imageData = {data} />
       </div>
     )
---
+++ src/Selection/Selection.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+export default class Selection extends Component {
+    imageDataArray = this.props.imageData;
+    imagePropertyArray = Array.from(new Set(this.imageDataArray.map(imageObj => imageObj[this.props.property])));
+    render() {
+        return (
+        
+            <select name = {this.props.selectionName}>
+            {
+
+                this.props.selectionName
+
+            }
+            {
+                this.imagePropertyArray.map(imageProperty => <option value = {imageProperty}>{imageProperty}</option>)
+            }
+            </select>
+        )
+    }
+}
